fa24efb9bc31210dda6786ef3267315feea9596b
Shazny <shaznykd@gmail.com>
2020-08-12 17:05:25 +0530

    Login files

---
+++ Identity/Login.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+session_destroy();
+?>
+
+<!doctype html>
+<html>
+<head>
+    <title> Login </title>
+    <link rel="stylesheet" type="text/css" href="../stylezz.css">
+    <!-- Bootstrap -->
+    <script src="../Plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</head>
+<body class="" background="../resources/globe-bg.jpg">
+<div class="login-box">
+
+    <img src="../Resources/icon.png" alt="Icon" class="icon" >
+    <h1 style="font-family:verdana;">Login </h1>
+    <form action="log-in.php" method="post">
+        <p style="font-family:verdana;">Username</p>
+        <input type="text" name="username" id="username" placeholder="Enter Username">
+        <p style="font-family:verdana;">Password</p>
+        <input type="password" name="password" id="password" placeholder="Enter Password">
+        <input type="submit" id="btn" value="Login">
+        <a href="ForgotPassword.php" style="font-family:verdana;">Forget Password</a>
+
+
+    </form>
+</div>
+
+</body>
+</html>
---
+++ Identity/log-in.php
@@ -0,0 +1,62 @@
+<?php
+//session_start();
+//$user = $_POST['username'];
+//$pass = $_POST['password'];
+
+
+include("../Common/config.php");
+//include("session.php");
+
+
+if (isset($_POST['username']))
+    $user = $_POST['username'];
+$pass = $_POST['password'];
+
+
+$result = mysqli_query($con, "select * from user where Username = '$user' and Password = '$pass' limit 1");
+//        while($row = mysqli_fetch_array($result))
+//        {
+//            print ("Login Successful");
+//            exit();
+//        }
+////        mysql
+///
+if (mysqli_num_rows($result) == 1) {
+
+    //print ("Login Successful");
+//    foreach ($result as $row) {
+////        $_SESSION['loggeduser'] = $row["M_Name"];
+////        $_SESSION['userid '] = $row["Mem_ID"];
+////        $_SESSION['useraddress'] = $row["Address"];
+////        $_SESSION['userstatus'] = $row["Acc_Status"];
+//
+//        if ($row['RoleID'] == "Admin") {
+//
+//            header('Location:Dashboard.php?e=Login Successful and session loaded');
+//
+//        } elseif ($row['RoleID'] == "Student") {
+//            header('Location:StdDashboard.php?e=Login Successful');
+//
+//        } else {
+//            header('Location:LectDashboard.html?e=Login Successful');
+//
+//        }
+
+//    }
+
+    header('Location:../Interfaces/PSC_Admin_Dashboard.php?e=Username or password Success');
+
+
+} else {
+    //print("Error: " . $sql . "<br>" . $con->error);
+//            print ("Wrong Credentials.....Maanga Madaya, Don't forget to create popup cookie in jquery ");
+//                exit();
+
+    header('Location:Login.php?e=Username or password failed');
+
+
+}
+$con->close();
+
+?>
+
