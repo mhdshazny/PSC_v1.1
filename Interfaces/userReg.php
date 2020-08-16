713e0a8daa5e49c574c43ac629649d996e011678
Shazny <shaznykd@gmail.com>
2020-08-16 10:57:22 +0530

    user reg back-end updated

--- Interfaces/userReg.php
+++ Interfaces/userReg.php
@@ -30,14 +30,32 @@ include("../Common/TopNavBar.php");
 
     </div>
 
-    <FORM action="userReg_dB.php" method="post" enctype="multipart/form-data">
+    <FORM action="userReg_dB.php" method="POST" enctype="">
         <div class="container-fluid" style="width: 80%">
             <div class="row">
                 <div class="col-sm-5">
                     <div class="form-group">
+                        <label for="userID" class="col-sm-3 control-label">User ID</label>
+                        <div class="col-sm-9">
+                            <input type="text" value="USER0001" id="userID" placeholder="userID" class="form-control" disabled>
+                        </div>
+                    </div>
+                    <div class="form-group">
                         <label for="firstName" class="col-sm-3 control-label">First Name</label>
                         <div class="col-sm-9">
                             <input type="text" id="firstName" placeholder="First Name" class="form-control" autofocus>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="addressLine1" class="col-sm-3 control-label">Address 1</label>
+                        <div class="col-sm-9">
+                            <input type="text" id="addressLine1" placeholder="Street address 1" class="form-control" autofocus>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="contactNo1" class="col-sm-3 control-label">Contact number</label>
+                        <div class="col-sm-9">
+                            <input type="text" id="contactNo1" placeholder="Contact number" class="form-control" autofocus>
                         </div>
                     </div>
                     <div class="form-group">
@@ -46,21 +64,16 @@ include("../Common/TopNavBar.php");
                             <input type="email" id="email" placeholder="Email" class="form-control" name= "email">
                         </div>
                     </div>
-                    <div class="form-group">
-                        <label for="password" class="col-sm-5 control-label">Confirm Password*</label>
-                        <div class="col-sm-9">
-                            <input type="password" id="password" placeholder="Password" class="form-control">
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label for="phoneNumber" class="col-sm-5 control-label">Phone number </label>
-                        <div class="col-sm-9">
-                            <input type="tel" id="phoneNumber" placeholder="Phone number" class="form-control">
-                            <span class="help-block" style="font-size: 80%">* Your phone number won't be disclosed anywhere </span>
-                        </div>
-                    </div>
                 </div>
+                <!--                Next column -->
                 <div class="col-sm-5">
+                    <div class="form-group">
+                        <label for="roleID" class="col-sm-3 control-label">User ID</label>
+                        <div class="col-sm-9">
+                            <input type="text" value="ROLE_EMP" id="roleID" placeholder="roleID" class="form-control" disabled>
+                        </div>
+                    </div>
+
                     <div class="form-group">
                         <label for="lastName" class="col-sm-3 control-label">Last Name</label>
                         <div class="col-sm-9">
@@ -68,9 +81,15 @@ include("../Common/TopNavBar.php");
                         </div>
                     </div>
                     <div class="form-group">
-                        <label for="password" class="col-sm-3 control-label">Password*</label>
+                        <label for="addressLine2" class="col-sm-3 control-label">Address 2</label>
                         <div class="col-sm-9">
-                            <input type="password" id="conPassword" placeholder="Password" class="form-control">
+                            <input type="text" id="addressLine2" placeholder="Street address 2 (Optional)" class="form-control" autofocus>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="contactNo2" class="col-sm-5 control-label">Contact number (Optional)</label>
+                        <div class="col-sm-9">
+                            <input type="text" id="contactNo2" placeholder="Contact number (Optional)" class="form-control" autofocus>
                         </div>
                     </div>
                     <div class="form-group">
@@ -79,32 +98,8 @@ include("../Common/TopNavBar.php");
                             <input type="date" id="birthDate" class="form-control">
                         </div>
                     </div>
-                    <div class="form-group">
-                        <label for="Height" class="col-sm-3 control-label">Height* </label>
-                        <div class="col-sm-9">
-                            <input type="number" id="height" placeholder="Please write your height in centimetres" class="form-control">
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label class="control-label col-sm-3">Gender</label>
-                        <div class="col-sm-6">
-                            <div class="row">
-                                <div class="col-sm-4">
-                                    <label class="radio-inline">
-                                        <input type="radio" name="gender" id="femaleRadio" value="Female">Female
-                                    </label>
-                                </div>
-                                <div class="col-sm-4">
-                                    <label class="radio-inline">
-                                        <input type="radio" name="gender" id="maleRadio" value="Male">Male
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-                    </div> <!-- /.form-group -->
-
                 </div>
-                <div class="col-sm-4">
+                <div class="col-sm-2">
                     <div class="form-group">
                         <label for="picture" class="col-sm-3 control-label">Picture</label>
                         <div class="col-sm-9">
@@ -113,20 +108,45 @@ include("../Common/TopNavBar.php");
                     </div>
                     <div class="form-group">
                         <div class="col-sm-9 col-sm-offset-3">
-                            <span class="help-block">*Required fields</span>
+                            <span class="help-block">* Required fields</span>
                         </div>
                     </div>
                 </div>
-                <br>
-                <div class="container" style="margin-left: 30%">
 
-                    <button type="submit" name="addUser" id="addUser" class="btn btn-primary btn-block" style="width: 50%; align-content: center">Register</button>
+                <br>
+            </div>
+
+
+            <br><br>
+            <div class="row">
+                <div class="col-sm-5">
+                    <div class="form-group">
+                        <label for="centerID" class="col-sm-3 control-label">Center ID</label>
+                        <div class="col-sm-9">
+                            <input type="text" id="centerID" placeholder="Center ID" class="form-control" autofocus>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-5">
+                    <div class="form-group">
+                        <label for="regionID" class="col-sm-3 control-label">Region ID</label>
+                        <div class="col-sm-9">
+                            <input type="text" id="regionID" placeholder="Region ID" class="form-control" autofocus>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <br><br>
         </div>
+
+        <br><br>
+        <div class="container" style="margin-left: 30%">
+            <button type="submit" name="addUser" id="addUser" class="btn btn-primary btn-block" style="width: 50%; align-content: center">Register</button>
+        </div>
+
+        <br><br>
     </FORM>
 </div>
+
 
 <?php
 include("../Common/Footer.php");
