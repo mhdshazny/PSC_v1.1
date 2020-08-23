44abe5bd4a3e511567160f066024adcdaf369bde
Himasha <hm.alexander99@gmail.com>
2020-08-23 06:57:31 +0530

    CO insert working

--- SK/chartData.js
+++ SK/chartData.js
@@ -84,6 +84,25 @@ let myLineChart1 = new Chart(ctxD, {
         datasets: [{
             data: [300, 50, 100, 40, 120],
             backgroundColor: ["#F7464A", "#46BFBD", "#FDB45C", "#949FB1", "#4D5360"],
+            hoverBackgroundColor: ["#FF5A5E", "#5AD3D1",
+                "#FFC870", "#A8B3C5", "#616774"]
+        }]
+    },
+    options: {
+        responsive: true
+    }
+});
+
+//store capacity
+
+let ctxS = document.getElementById("myChart4").getContext('2d');
+let myChart4 = new Chart(ctxS, {
+    type: 'doughnut',
+    data: {
+        labels: ["Red", "Green", "Yellow", "Grey", "Dark Grey"],
+        datasets: [{
+            data: [300, 50, 100, 40, 120],
+            backgroundColor: ["#F7464A", "#46BFBD", "#FDB45C", "#949FB1", "#4D5360"],
             hoverBackgroundColor: ["#FF5A5E", "#5AD3D1", "#FFC870", "#A8B3C5", "#616774"]
         }]
     },
