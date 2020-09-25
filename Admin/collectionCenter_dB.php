c50919049b8c4ba5639e9bf84e7142abb1161b94
Himasha <hm.alexander99@gmail.com>
2020-09-25 21:46:21 +0530

    stuff

--- Admin/collectionCenter_dB.php
+++ Admin/collectionCenter_dB.php
@@ -47,34 +47,29 @@ if (isset($_POST['addCenter'])) {
 
 //Update
 
-    if (isset($_POST['updateFarmer'])) {
+    if (isset($_POST['updateCenter'])) {
 
-        $UP_farmerID = $_POST['farmerID'];
-        $UP_centerID = $_POST['centerID'];
 
-        $UP_fName = $_POST['firstName'];
-        $UP_lName = $_POST['lastName'];
-        $UP_address1 = $_POST['addressLine1'];
-        $UP_address2 = $_POST['addressLine2'];
-        $UP_contact = $_POST['contactNo1'];
-        $UP_contact2 = $_POST['contactNo2'];
-        $UP_email = $_POST['email'];
-        $UP_gender = $_POST['gender'];
-        $UP_isActive = "0";
-        $UP_isActive = $_POST['isActive'];
-        $UP_region = $_POST['regionName'];
-        $UP_NIC = $_POST['NIC'];
-        $landArea = $_POST['landArea'];
+        $centerID = $_POST['centerID'];
+
+        $CenterName = $_POST['CenterName'];
+        $Capacity = $_POST['Capacity'];
+        $bankAccID = $_POST['bankAccID'];
+        $contact = $_POST['contactNo1'];
+
+        $CenterName= $_POST['CenterName'];
+        $amount = $_POST['amount'];
+
 
 
         if (!empty($UP_farmerID) && !empty($UP_fName) && !empty($UP_lName) && !empty($UP_address1) && !empty($UP_contact) && !empty($UP_gender) && !empty($landArea)) {
-            $sql = "UPDATE `tbl_farmer` SET `firstName`='$UP_fName',`lastName`='$UP_lName',`addressLine1`='$UP_address1',`addressLine2`='$UP_address2',`contactNo1`='$UP_contact',`contactNo2`='$UP_contact2', `centerID`='$UP_centerID',`gender`='$UP_gender',`isActive`='$UP_isActive',`landArea`='$landArea' WHERE `farmerID`='$UP_farmerID' ";
+            $sql = "UPDATE `tbl_collectioncenter` SET `firstName`='$UP_fName',`lastName`='$UP_lName',`addressLine1`='$UP_address1',`addressLine2`='$UP_address2',`contactNo1`='$UP_contact',`contactNo2`='$UP_contact2', `centerID`='$UP_centerID',`gender`='$UP_gender',`isActive`='$UP_isActive',`landArea`='$landArea' WHERE `farmerID`='$UP_farmerID' ";
 
 //                `profilePic`, `roleID`, `addressLine1`, `addressLine2`, `contactNo1`, `contactNo2`, `email`, `centerID`, `dob`, `gender`, `isActive`,`password`
             if ($con->query($sql) === TRUE) {
 //                     if (move_uploaded_file($_FILES["picture"]["tmp_name"], $target_file)) {
                 mysqli_commit($con);
-                header('Location: Farmer_Register.php?e=data updated');
+                header('Location: CollectionCenter.php?e=data updated');
 //                     } else {
 //                         echo "Sorry, there was an error uploading your file.";
 //                     }
@@ -88,12 +83,12 @@ if (isset($_POST['addCenter'])) {
 
                 //This will redirect to same page and it'll show the message above url//
 
-                header('Location: Farmer_Register.php?e=Up Wrong Credentials/Query stopped working');
+                header('Location: CollectionCenter.php?e=Up Wrong Credentials/Query stopped working');
             }
 
 
         } else {
-            header('Location: Farmer_Register.php?e=credentials Missing6');
+            header('Location: CollectionCenter.php?e=credentials Missing6');
 
         }
 
