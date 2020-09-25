2ca36b6e0358ee7ac5bdd75e2a048e3280571392
Shazny <shaznykd@gmail.com>
2020-09-25 21:58:28 +0530

    Top Nav Bar Updated

--- CM_HO/SideNavBar.php
+++ CM_HO/SideNavBar.php
@@ -33,7 +33,7 @@
 <!--            -->
             <li class="nav-item">
                 <a class="nav-link text-white" href="setIssueOrders.php">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file"><path d="M13 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V9z"></path><polyline points="13 2 13 9 20 9"></polyline></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"></circle><path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3"></path><line x1="12" y1="17" x2="12.01" y2="17"></line></svg>
                     &nbsp;Set Issue Orders
                 </a>
             </li>
